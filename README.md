9697a8e9eee9bfe8e2ba3ff0b065b5ec88e00e96
Victor GRENU <victor.grenu@gmail.com>
2018-12-23 14:45:53 +0100

    first attempt

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ S3 Monitor intend to check the last object(key) creation date in S3, and if its 
 
 ## Technical details
 
-* Using AWS Lambda (Python)
+* Uses AWS Lambda function (Python)
 * Scheduled (daily) with cron
-* Uses AWS Simple Email Service for Notifications
+* Uses AWS Simple Email Service (SES) for Emails Notifications
 
-## Used Documentation
+## Documentation
 
 * https://stackoverflow.com/questions/9679344/how-can-i-get-last-modified-datetime-of-s3-objects-with-boto
 * https://docs.aws.amazon.com/ses/latest/DeveloperGuide/send-using-sdk-python.html
+* https://boto3.amazonaws.com/v1/documentation/api/latest/guide/migrations3.html
---
+++ s3monitor.py
@@ -0,0 +1,110 @@
+import time
+import boto3
+from botocore.exceptions import ClientError
+import botocore
+from datetime import datetime
+import datetime
+from time import mktime
+from dateutil.parser import parse
+
+# Email Setting (SES)
+# Replace sender@example.com with your "From" address.
+# This address must be verified with Amazon SES.
+SENDER = "Alfred Backup <victor.grenu@gmail.com>"
+
+# Replace recipient@example.com with a "To" address. If your account 
+# is still in the sandbox, this address must be verified.
+RECIPIENT = "victor.grenu@gmail.com"
+
+# Specify a configuration set. If you do not want to use a configuration
+# set, comment the following variable, and the 
+# ConfigurationSetName=CONFIGURATION_SET argument below.
+#CONFIGURATION_SET = "ConfigSet"
+
+#If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+AWS_REGION = "eu-west-1"
+
+#The subject line for the email.
+SUBJECT = "Alfred Backup Failed"
+
+#The email body for recipients with non-HTML email clients.
+BODY_TEXT = ("Alert - Warning\r\n"
+             "Jeedom Backup Failed"
+             "Alfred"
+            )
+            
+#The HTML body of the email.
+BODY_HTML = """<html>
+<head></head>
+<body>
+  <h1>Alfred Backup</h1>
+  <p>The backup failed today</p>
+  <p>
+</body>
+</html>
+            """            
+
+#The character encoding for the email.
+CHARSET = "UTF-8"
+
+#Create a new SES resource and specify a region.
+client = boto3.client('ses',region_name=AWS_REGION)
+
+#Get Today's date
+today = datetime.date.today()
+
+#Get Objects date
+s3 = boto3.resource('s3',region_name=AWS_REGION)
+bucket = s3.Bucket('zoph.backup')
+objs = bucket.objects.filter(Prefix='Jeedom').limit(1)
+
+try:
+    for obj in objs:
+        lastobjectdate = (obj.last_modified).date()
+except botocore.exceptions.ClientError as e:
+    error_code = e.response['Error']['Code']
+    if error_code == '404':
+        exists = False
+
+#Compare with defined date
+if today != lastobjectdate:
+    print("OK")
+else:
+    #SES Notification
+    #Try to send the email.
+    try:
+    #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+            # If you are not using a configuration set, comment or delete the
+            # following line
+            #ConfigurationSetName=CONFIGURATION_SET,
+        )
+    # Display an error if something goes wrong.	
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
+        print ("KO")
