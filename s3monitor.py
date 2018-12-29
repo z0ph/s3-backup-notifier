2c7aef5c80ff430d2fb8f36aa83e311888ab42cc
Victor GRENU <victor.grenu@gmail.com>
2018-12-29 09:41:08 +0100

    better approach

--- s3monitor.py
+++ s3monitor.py
@@ -3,83 +3,77 @@ from botocore.exceptions import ClientError
 import botocore
 import datetime
 
-# Email Setting (SES)
-# Replace sender@example.com with your "From" address.
-# This address must be verified with Amazon SES.
-SENDER = "Alfred Backup <victor.grenu@gmail.com>"
 
-# Replace recipient@example.com with a "To" address. If your account 
-# is still in the sandbox, this address must be verified.
-RECIPIENT = "victor.grenu@gmail.com"
+# Get Today's date
+today = datetime.date.today()
+todaystr = str(today)
 
-# Specify a configuration set. If you do not want to use a configuration
-# set, comment the following variable, and the 
-# ConfigurationSetName=CONFIGURATION_SET argument below.
-#CONFIGURATION_SET = "ConfigSet"
-
-#If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
 AWS_REGION = "eu-west-1"
-
-# Subject line for the email.
-SUBJECT = "Alfred Backup Failed"
-
-# Email body for recipients with non-HTML email clients.
-BODY_TEXT = ("Alert - Warning\r\n"
-             "Jeedom Backup Failed"
-             "Alfred"
-            )
-            
-# HTML body of the email.
-BODY_HTML = """<html>
-<head></head>
-<body>
-  <h1>Alfred Jeedom Backup</h1>
-  <p>Jeedom backup failed today</p>
-  <p>The file is not present on S3</p>
-  <p>Alfred</p>
-</body>
-</html>
-            """            
-
-# Character encoding for the email.
-CHARSET = "UTF-8"
 
 # Create a new SES resource and specify a region.
 client = boto3.client('ses',region_name=AWS_REGION)
 
-# Get Today's date
-today = datetime.date.today()
-
 # Get Objects date
 s3 = boto3.resource('s3',region_name=AWS_REGION)
 bucket = s3.Bucket('zoph.backup')
-objs = bucket.objects.filter(Prefix='Jeedom').limit(1)
+objs = bucket.objects.filter(Prefix='Jeedom').all()
 
 
 def get_object_check_alarm():
     try:
         for obj in objs:
-            print(obj)
             lastobjectdate = (obj.last_modified).date()
+            lastobjectstr = str(lastobjectdate)
     except botocore.exceptions.ClientError as e:
         error_code = e.response['Error']['Code']
         if error_code == '404':
-            print("There is no file")
+            print("There is no file in this bucket")
 
     # Compare with defined date
     if today == lastobjectdate:
-        print(today)
-        print(lastobjectdate)
-        print("OK, lastest file comes from today")
+        print("OK, latest file comes from " +lastobjectstr+ ". We are: " +todaystr)
     else:
-        print(today)
-        print(lastobjectdate)
-        print("Mail sent")
         # SES Notification
         # Try to send the email.
-        '''
         try:
-        # Provide the contents of the email.
+            # Email Setting (SES)
+            # Replace sender@example.com with your "From" address.
+            # This address must be verified with Amazon SES.
+            SENDER = "Alfred Backup <victor.grenu@gmail.com>"
+
+            # Replace recipient@example.com with a "To" address. If your account 
+            # is still in the sandbox, this address must be verified.
+            RECIPIENT = "victor.grenu@gmail.com"
+
+            # Specify a configuration set. If you do not want to use a configuration
+            # set, comment the following variable, and the 
+            # ConfigurationSetName=CONFIGURATION_SET argument below.
+            #CONFIGURATION_SET = "ConfigSet"
+
+            # Subject line for the email.
+            SUBJECT = "Alfred Backup Failed"
+
+            # Email body for recipients with non-HTML email clients.
+            BODY_TEXT = ("Alert - Warning\r\n"
+                        "Jeedom Backup Failed"
+                        "Alfred"
+                        )
+                        
+            # HTML body of the email.
+            BODY_HTML = """<html>
+            <head></head>
+            <body>
+            <h1>Alfred Jeedom Automated Backup</h1>
+            <p>Jeedom backup failed on """+todaystr+"""</p>
+            <p>The latest key in S3 comes from """+lastobjectstr+"""</p>
+            <p>Alfred</p>
+            </body>
+            </html>
+                        """            
+
+            # Character encoding for the email.
+            CHARSET = "UTF-8"
+            # Provide the contents of the email.
             response = client.send_email(
                 Destination={
                     'ToAddresses': [
@@ -111,16 +105,14 @@ def get_object_check_alarm():
         except ClientError as e:
             print(e.response['Error']['Message'])
         else:
+            print("We are: " +todaystr+ ". file is old: " +lastobjectstr)
             print("Email sent! Message ID:"),
             print(response['MessageId'])
-            print ("KO, file is too old, sending alert email")
-        '''
 
 
 def main():
     get_object_check_alarm()
 
 # Run locally
-
 
 main()# only for testing purpose
